2a41da6a4d95690056259022c4d7168d98380793
Simon Emms <simon@simonemms.com>
2025-08-01 14:30:11 +0000

    feat: create basic app

--- cmd/root.go
+++ cmd/root.go
@@ -16,16 +16,44 @@ limitations under the License.
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var rootOpts struct {
+	FilePath string
+	LogLevel string
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "temporal-serverless-workflow",
 	Version: Version,
 	Short:   "Build Temporal workflows with Serverless Workflow",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		level, err := zerolog.ParseLevel(rootOpts.LogLevel)
+		if err != nil {
+			return err
+		}
+		zerolog.SetGlobalLevel(level)
+
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		// Load the workflow file
+		data, err := os.ReadFile(filepath.Clean(rootOpts.FilePath))
+		if err != nil {
+			log.Fatal().Err(err).Str("file", rootOpts.FilePath).Msg("Unable to load workflow file")
+		}
+
+		fmt.Println(string(data))
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -35,4 +63,26 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func init() {
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("WF")
+
+	rootCmd.Flags().StringVarP(
+		&rootOpts.FilePath,
+		"file",
+		"f",
+		viper.GetString("file"),
+		"Path to workflow file",
+	)
+
+	viper.SetDefault("log_level", zerolog.InfoLevel.String())
+	rootCmd.PersistentFlags().StringVarP(
+		&rootOpts.LogLevel,
+		"log-level",
+		"l",
+		viper.GetString("log_level"),
+		fmt.Sprintf("log level: %s", "Set log level"),
+	)
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,29 @@ module github.com/mrsimonemms/temporal-serverless-workflow
 
 go 1.24.2
 
-require github.com/spf13/cobra v1.9.1
+require (
+	github.com/rs/zerolog v1.34.0
+	github.com/spf13/cobra v1.9.1
+	github.com/spf13/viper v1.20.1
+)
 
 require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/sagikazarmark/locafero v0.10.0 // indirect
+	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
+	github.com/spf13/afero v1.14.0 // indirect
+	github.com/spf13/cast v1.9.2 // indirect
 	github.com/spf13/pflag v1.0.7 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/text v0.27.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
